7321dc7dece52bc55978a8a850d958f3b1b2a7ae
LoraGotseva <lora.gotseva@abv.bg>
2025-01-03 16:12:01 +0200

    added images and data

---
+++ pet-booking-app/src/data/data.js
@@ -0,0 +1,78 @@
+import Sofia from '../../public/assets/sofia.jpg'
+import Ruse from '../../public/assets/ruse.jpg'
+import Varna from '../../public/assets/varna.jpg'
+import Plovdiv from '../../public/assets/plovdiv.jpg'
+import Burgas from '../../public/assets/burgas.jpg'
+import StaraZagora from '../../public/assets/starazagora.jpg'
+import Pleven from '../../public/assets/pleven.jpg'
+import Sliven from '../../public/assets/sliven.jpg'
+
+export const popularCities = [
+    {
+        city: "Sofia",
+        image: Sofia
+    },
+    {
+        city: "Ruse",
+        image: Ruse
+    },
+    {
+        city: "Varna",
+        image: Varna
+    },
+    {
+        city: "Plovdiv",
+        image: Plovdiv
+    },
+]
+
+export const optionTypes = [
+    { text: "Luxury", value: "luxury" },
+    { text: "Budget", value: "budget" },
+    { text: "3 Stars", value: "threeStars" },
+    { text: "4 Stars", value: "fourStars" },
+    { text: "5 Stars", value: "fiveStars" },
+]
+
+export const optionLocations = [
+    {
+        city: "Ruse",
+        value: "ruse",
+        image: Ruse
+    },
+    {
+        city: "Burgas",
+        value: "burgas",
+        image: Burgas
+    },
+    {
+        city: "Stara Zagora",
+        value: "starazagora",
+        image: StaraZagora
+    },
+    {
+        city: "Sofia",
+        value: "sofia",
+        image: Sofia
+    },
+    {
+        city: "Varna",
+        value: "varna",
+        image: Varna
+    },
+    {
+        city: "Pleven",
+        value: "pleven",
+        image: Pleven
+    },
+    {
+        city: "Sliven",
+        value: "sliven",
+        image: Sliven
+    },
+    {
+        city: "Plovdiv",
+        value: "plovdiv",
+        image: Plovdiv
+    },
+]
